18ff1d98f1b3d94f6d20059a92c3aed2306b75e1
Kim-SuYeong <74577728+Kim-SuYeong@users.noreply.github.com>
2023-11-27 22:36:56 +0900

    Create ex100_26.java

---
+++ java_ex100/ex100_26.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ex100_26 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		boolean a = scan.nextBoolean();
+		
+		boolean b = !a;
+		System.out.println(b);
+		
+		scan.close();
+	}
+
+}
